71a93b3b129f6e27a69cb9f02cbf17775b81bc53
Josie Park <Josie.Park@ukhsa.gov.uk>
2022-12-08 14:58:46 +0000

    first commit

---
+++ pipTutorial/__main__.py
@@ -0,0 +1,4 @@
+import pipTutorial.funcs
+
+if __name__ == "__main__":
+    print(pipTutorial.funcs.hello_world())
---
+++ pipTutorial/funcs.py
@@ -0,0 +1,8 @@
+def hello_world():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
+
+    return "hello cat"
